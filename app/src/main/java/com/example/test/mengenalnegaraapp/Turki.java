7bb5fc4132321dd50a0d83d2ab83445235aec6d1
mynameisnabil <43395606+mynameisnabil@users.noreply.github.com>
2018-10-12 09:01:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test/mengenalnegaraapp/Turki.java
@@ -0,0 +1,13 @@
+package com.example.test.mengenalnegaraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Turki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turki);
+    }
+}
